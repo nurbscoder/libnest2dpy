19fa5ccdceef719d4a28c55fc445fa022014e936
Markus Frings <coder@frings-mail.de>
2023-01-01 20:42:59 +0100

    Current status

---
+++ build.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+import os
+import pathlib
+import platform
+import subprocess
+import sys
+import sysconfig
+from distutils.command.build_ext import build_ext
+from distutils.version import LooseVersion
+from typing import Any
+
+from setuptools import Extension
+
+
+class CMakeExtension(Extension):
+    name: str
+
+    def __init__(self, name: str, sourcedir: str="") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class ExtensionBuilder(build_ext):
+    def run(self) -> None:
+        self.validate_cmake()
+        super().run()
+
+    def build_extension(self, ext: Extension) -> None:
+        if isinstance(ext, CMakeExtension):
+            self.build_cmake_extension(ext)
+        else:
+            super().build_extension(ext)
+
+    def validate_cmake(self) -> None:
+        cmake_extensions = [x for x in self.extensions if isinstance(x, CMakeExtension)]
+        if len(cmake_extensions) > 0:
+            try:
+                out = subprocess.check_output(["cmake", "--version"])
+            except OSError:
+                raise RuntimeError(
+                    "CMake must be installed to build the following extensions: "
+                    + ", ".join(e.name for e in cmake_extensions)
+                )
+            if platform.system() == "Windows":
+                cmake_version = LooseVersion(re.search(r"version\s*([\d.]+)", out.decode()).group(1))  # type: ignore
+                if cmake_version < "3.4.0":
+                    raise RuntimeError("CMake >= 3.4.0 is required on Windows")
+
+    def build_cmake_extension(self, ext: CMakeExtension) -> None:
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir, "-DPYTHON_EXECUTABLE=" + sys.executable]
+
+        cfg = "Debug" if self.debug else "Release"
+        # cfg = 'Debug'
+        build_args = ["--config", cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
+            if sys.maxsize > 2 ** 32:
+                cmake_args += ["-A", "x64"]
+            build_args += ["--", "/m"]
+        else:
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+            build_args += ["--", "-j4"]
+        cmake_args += ["-DPYTHON_INCLUDE_DIR={}".format(sysconfig.get_path("include"))]
+        cmake_args += ["-DRP_ENABLE_DOWNLOADING=ON"]
+        cmake_args += ["-DLIBNEST2D_HEADER_ONLY=OFF"]
+
+        env = os.environ.copy()
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get("CXXFLAGS", ""), self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=self.build_temp)
+
+def build(setup_kwargs: dict[str, Any]) -> None:
+    cmake_modules = [CMakeExtension("libnest2dpy", sourcedir=str(pathlib.Path(__file__).parent.resolve()))]
+
+    setup_kwargs.update(
+        {
+            "ext_modules": cmake_modules,
+            "cmdclass": dict(build_ext=ExtensionBuilder),
+            "zip_safe": False,
+        }
+    )
