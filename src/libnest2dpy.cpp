de0547ab20a9f72d84dde839c1ef50d85b79cd7d
Markus Frings <coder@frings-mail.de>
2023-01-03 20:39:19 +0100

    Adapt library name
    Module name must match the module name from CMake.

--- src/libnest2dpy.cpp
+++ src/libnest2dpy.cpp
@@ -1,9 +1,8 @@
 #include <pybind11/pybind11.h>
 
-
 namespace py = pybind11;
 
-PYBIND11_MODULE(libnest2dpy, m)
+PYBIND11_MODULE(_libnest2dpy, m)
 {
-    m.doc() = "Python wrapper for 2D irregular bin packaging and nesting library";
+    m.doc() = "Internal module of python wrapper for 2D irregular bin packaging and nesting library";
 }
