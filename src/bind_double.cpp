6a2f3907163f45481aed89b114d103c6f8f73030
Markus Frings <coder@frings-mail.de>
2023-01-07 13:48:24 +0100

    Add macro for call guard policy
    It is used in almost each function. Therefore, it is shorter to use a
    macro.

--- src/bind_double.cpp
+++ src/bind_double.cpp
@@ -1,6 +1,10 @@
+#include "bind_double.hpp"
+
 #include <pybind11/pybind11.h>
 
 #include <libnest2d/libnest2d.hpp>
+
+#include "common.hpp"
 
 namespace py = pybind11;
 
@@ -8,10 +12,11 @@ namespace libnest2dpy {
 using namespace libnest2d;
 void bind_double(py::module& m) {
   py::class_<Double>(m, "Double")
-      .def(py::init<>(), py::call_guard<py::gil_scoped_release>())
-      .def(py::init<double>(), py::arg("val"),
-           py::call_guard<py::gil_scoped_release>())
-      .def("__float__",
-           [](const Double& self) { return static_cast<double>(self); });
+      .def(py::init<>(), CG_RELEASE)
+      .def(py::init<double>(), py::arg("val"), CG_RELEASE)
+      .def(
+          "__float__",
+          [](const Double& self) { return static_cast<double>(self); },
+          CG_RELEASE);
 }
 }  // namespace libnest2dpy
